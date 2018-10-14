1ebad0902da1c560feb04c5b408f8d41dc1c1a07
wasimakram-mulla <wasim3ace@gmail.com>
2018-10-14 20:09:28 +0530

    dashboard updated

--- src/app/dashboardcater/dashboardcater.component.ts
+++ src/app/dashboardcater/dashboardcater.component.ts
@@ -1,5 +1,6 @@
 import { OrderService } from './../order.service';
 import { Component, OnInit } from '@angular/core';
+import { MenuService } from '../menu.service';
 
 @Component({
   selector: 'app-dashboardcater',
@@ -8,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardcaterComponent implements OnInit {
   receivedOrders: any;
+  allMenus: any;
+  deliveryOut: any;
+  pendingDelivery: any;
+  topHotels: any;
+  topSuppliers: any;
 
-  constructor(private _order: OrderService) { }
+  constructor(private _order: OrderService, private _menu: MenuService) { }
 
   ngOnInit() {
     this._order.getAllOpenOrders().subscribe(response => {
@@ -20,7 +26,17 @@ export class DashboardcaterComponent implements OnInit {
     },
     err =>{
       console.log(err);
-    });  
+    });
+
+    this._menu.getAllMenus().subscribe(response =>{
+      if(response){
+        console.log("getAllMenus", response);
+        this.allMenus = response["data"];
+      }
+    },
+    err => {
+      console.log(err);
+    });
   }
 
 }
